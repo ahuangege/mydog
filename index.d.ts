7d112627c1935a07552bd9f7cecf843bc003883c
huangjizhong <1124831983@qq.com>
2018-12-29 11:14:27 +0800

    fdsaf

--- index.d.ts
+++ index.d.ts
@@ -132,7 +132,7 @@ export interface Application {
     /**
      * 编码解码回调
      */
-    set(key: "encodeDecodeConfig", value: { "encode": Function, "decode": Function }): void
+    set(key: "encodeDecodeConfig", value: { "encode": (cmdId: number, data: any) => Buffer, "decode": (cmdId: number, data: Buffer) => any }): void
 
     /**
      * 前端连接服务器配置
