81bd82f001a8e53ef8303cbc4f1e9cff2d9ff499
ahuangege <1124831983@qq.com>
2020-09-27 20:12:16 +0800

    template错误

--- template/server/src/config/cmd.ts
+++
@@ -1,8 +0,0 @@
-export const enum cmd {
-    /**
-     * 聊天
-     */
-    onChat = "onChat",
-    onLeave = "onLeave",
-    onNewPlayer = "onNewPlayer",
-}
--- template/server/src/config/sys/master.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    "development": {
-        "id": "master-server-1", "host": "127.0.0.1", "port": 3005
-    },
-    "production": {
-        "id": "master-server-1", "host": "127.0.0.1", "port": 3005
-    }
-}
