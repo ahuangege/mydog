c4972dc20eae896f1fc67be9cd063f378c1aced2
huangjizhong <1124831983@qq.com>
2020-11-13 20:29:36 +0800

    cli

--- src/cli.ts
+++ src/cli.ts
@@ -163,58 +163,6 @@ program.command('*')
 program.parse(process.argv);
 
 
-function init() {
-    let path = process.cwd();
-    emptyDirectory(path, function (empty) {
-        if (empty) {
-            process.stdin.destroy();
-            createApplicationAt(path);
-        } else {
-            confirm('Destination is not empty, continue? (y/n) [no] ', function (force) {
-                process.stdin.destroy();
-                if (force) {
-                    createApplicationAt(path);
-                } else {
-                    abort('Fail to init a project');
-                }
-            });
-        }
-    });
-}
-
-
-function createApplicationAt(ph: string) {
-    copy(path.join(__dirname, '../template'), ph);
-}
-
-
-function emptyDirectory(path: string, fn: (isEmpth: boolean) => void) {
-    fs.readdir(path, function (err, files) {
-        if (err && 'ENOENT' !== err.code) {
-            abort(FILEREAD_ERROR);
-        }
-        fn(!files || !files.length);
-    });
-}
-
-function confirm(msg: string, fn: (yes: boolean) => void) {
-    prompt(msg, function (val) {
-        fn(/^ *y(es)?/i.test(val));
-    });
-}
-
-
-function prompt(msg: string, fn: (data: string) => void) {
-    if (' ' === msg[msg.length - 1]) {
-        process.stdout.write(msg);
-    } else {
-        console.log(msg);
-    }
-    process.stdin.setEncoding('ascii');
-    process.stdin.once('data', function (data) {
-        fn(data.toString());
-    }).resume();
-}
 
 function abort(str: string = "") {
     console.error(str);
@@ -222,30 +170,82 @@ function abort(str: string = "") {
 }
 
 
-function copy(origin: string, target: string) {
-    if (!fs.existsSync(origin)) {
-        abort(origin + 'does not exist.');
-    }
-    if (!fs.existsSync(target)) {
-        fs.mkdirSync(target);
-        console.log('   create :  ' + target);
-    }
-    fs.readdir(origin, function (err, datalist) {
-        if (err) {
-            abort(FILEREAD_ERROR);
-        }
-        for (let i = 0; i < datalist.length; i++) {
-            let oCurrent = path.resolve(origin, datalist[i]);
-            let tCurrent = path.resolve(target, datalist[i]);
-            if (fs.statSync(oCurrent).isFile()) {
-                fs.writeFileSync(tCurrent, fs.readFileSync(oCurrent, ''), '');
-                console.log('   create :  ' + tCurrent);
-            } else if (fs.statSync(oCurrent).isDirectory()) {
-                copy(oCurrent, tCurrent);
-            }
+function init() {
+    let pathStr = process.cwd();
+    emptyDirectory(pathStr, function (empty) {
+        if (empty) {
+            process.stdin.destroy();
+            createApplicationAt(pathStr);
+        } else {
+            confirm('Destination is not empty, continue? (y/n) [no] ', function (force) {
+                process.stdin.destroy();
+                if (force) {
+                    createApplicationAt(pathStr);
+                } else {
+                    abort('Fail to init a project');
+                }
+            });
         }
     });
+
+    function confirm(msg: string, fn: (yes: boolean) => void) {
+        prompt(msg, function (val) {
+            fn(/^ *y(es)?/i.test(val));
+        });
+        function prompt(msg: string, fn: (data: string) => void) {
+            if (' ' === msg[msg.length - 1]) {
+                process.stdout.write(msg);
+            } else {
+                console.log(msg);
+            }
+            process.stdin.setEncoding('ascii');
+            process.stdin.once('data', function (data) {
+                fn(data.toString());
+            }).resume();
+        }
+    }
+
+    function createApplicationAt(ph: string) {
+        copy(path.join(__dirname, '../template'), ph);
+        function copy(origin: string, target: string) {
+            if (!fs.existsSync(origin)) {
+                abort(origin + 'does not exist.');
+            }
+            if (!fs.existsSync(target)) {
+                fs.mkdirSync(target);
+                console.log('   create :  ' + target);
+            }
+            fs.readdir(origin, function (err, datalist) {
+                if (err) {
+                    abort(FILEREAD_ERROR);
+                }
+                for (let i = 0; i < datalist.length; i++) {
+                    let oCurrent = path.resolve(origin, datalist[i]);
+                    let tCurrent = path.resolve(target, datalist[i]);
+                    if (fs.statSync(oCurrent).isFile()) {
+                        fs.writeFileSync(tCurrent, fs.readFileSync(oCurrent, ''), '');
+                        console.log('   create :  ' + tCurrent);
+                    } else if (fs.statSync(oCurrent).isDirectory()) {
+                        copy(oCurrent, tCurrent);
+                    }
+                }
+            });
+        }
+    }
+
+    function emptyDirectory(path: string, fn: (isEmpth: boolean) => void) {
+        fs.readdir(path, function (err, files) {
+            if (err && 'ENOENT' !== err.code) {
+                abort(FILEREAD_ERROR);
+            }
+            fn(!files || !files.length);
+        });
+    }
+
 }
+
+
+
 
 
 function start(opts: { "env": string, "daemon": boolean, "serverIds": string }) {
@@ -349,6 +349,84 @@ function list(opts: any) {
             return 1;
         }
     };
+
+
+    function mydogListPrint(appName: string, env: string, infoArr: string[][]) {
+        let consoleMaxColumns = process.stdout.columns - 2;
+        let nameEnv = "  appName: " + appName + "    env: " + env;
+        console.log("\x1b[35m" + getRealStr(nameEnv) + "\x1b[0m");
+
+        let widthArr: number[][] = [];  // 每个字段的控制台宽度
+        let columnWidth: number[] = []; // 每列的最大宽度
+        let titleLen = infoArr[0].length;
+        for (let i = 0; i < titleLen; i++) {
+            columnWidth.push(0);
+        }
+        for (let i = 0; i < infoArr.length; i++) {
+            let one = infoArr[i];
+            if (one.length > titleLen) {
+                one.splice(titleLen);
+            } else if (one.length < titleLen) {
+                for (let j = titleLen - one.length - 1; j >= 0; j--) {
+                    one.push("");
+                }
+            }
+            let tmpArr: number[] = [];
+            for (let j = 0; j < titleLen; j++) {
+                one[j] = one[j].toString();
+                let tmpLen = getDisplayLength(one[j]);
+                tmpArr.push(tmpLen);
+                if (tmpLen > columnWidth[j]) {
+                    columnWidth[j] = tmpLen;
+                }
+            }
+            widthArr[i] = tmpArr;
+        }
+        for (let i = 0; i < titleLen; i++) {
+            columnWidth[i] += 3;
+        }
+
+        for (let i = 0; i < infoArr.length; i++) {
+            let one = infoArr[i];
+            let tmpWidthArr = widthArr[i];
+            for (let j = 0; j < titleLen; j++) {
+                one[j] += " ".repeat(columnWidth[j] - tmpWidthArr[j]);
+            }
+            if (i === 0) {
+                console.log("\x1b[31m" + getRealStr(one.join("")) + "\x1b[0m");
+            } else {
+                console.log(getRealStr(one.join("")));
+            }
+        }
+
+
+
+        function getRealStr(str: string) {
+            while (getDisplayLength(str) > consoleMaxColumns) {
+                str = str.substring(0, str.length - 2);
+            }
+            return str;
+        }
+
+
+        //获得字符串实际长度，中文2，英文1
+        //控制台中中文占用2个英文字符的宽度
+        function getDisplayLength(str: string) {
+            let realLength = 0, len = str.length, charCode = -1;
+            for (var i = 0; i < len; i++) {
+                charCode = str.charCodeAt(i);
+                if (charCode >= 0 && charCode <= 128) {
+                    realLength += 1;
+                } else {
+                    realLength += 2;
+                }
+
+            }
+            return realLength;
+        }
+
+    }
+
 }
 
 
@@ -393,82 +471,72 @@ function removeT(opts: any) {
 
 
 
-function mydogListPrint(appName: string, env: string, infoArr: string[][]) {
+function cmd() {
+    let routePath = "../config/sys/route.ts";
+    let serverPath = "../config/cmd.ts";
 
-    let consoleMaxColumns = process.stdout.columns - 2;
-    let nameEnv = "  appName: " + appName + "    env: " + env;
-    console.log("\x1b[35m" + getRealStr(nameEnv) + "\x1b[0m");
+    let readStream = fs.createReadStream(path.join(__dirname, routePath));
+    let read_l = readline.createInterface({ "input": readStream });
 
-    let widthArr: number[][] = [];  // 每个字段的控制台宽度
-    let columnWidth: number[] = []; // 每列的最大宽度
-    let titleLen = infoArr[0].length;
-    for (let i = 0; i < titleLen; i++) {
-        columnWidth.push(0);
-    }
-    for (let i = 0; i < infoArr.length; i++) {
-        let one = infoArr[i];
-        if (one.length > titleLen) {
-            one.splice(titleLen);
-        } else if (one.length < titleLen) {
-            for (let j = titleLen - one.length - 1; j >= 0; j--) {
-                one.push("");
+    let hasStart = false;
+    let cmdObjArr: { "cmd": string, "note": string }[] = [];
+
+    read_l.on("line", function (line) {
+        line = line.trim();
+        if (line === "") {
+            return;
+        }
+        if (!hasStart) {
+            if (line.indexOf("export") === 0) hasStart = true;
+            return;
+        }
+        if (line.indexOf("]") === 0) {
+            serverCmd();
+            read_l.close();
+            return;
+        }
+        if (line.indexOf('"') !== 0) {
+            return;
+        }
+        line = line.substring(1);
+        let index = line.indexOf('"');
+        if (index === -1) {
+            return;
+        }
+
+        let cmd = line.substring(0, index);
+        let note = "";
+        index = line.indexOf("//");
+        if (index !== -1) {
+            note = line.substring(index + 2).trim();
+        }
+        cmdObjArr.push({ "cmd": cmd, "note": note });
+    });
+
+    read_l.on("close", function () {
+        console.log("build route ok!");
+    });
+
+
+    function serverCmd() {
+        let endStr = `export const enum cmd {\n`
+        let index = 0;
+        for (let one of cmdObjArr) {
+            if (one.cmd.indexOf('.') === -1) {
+                if (one.note) {
+                    endStr += `\t/**\n\t * ${one.note}\n\t */\n`;
+                }
+                endStr += `\t${one.cmd} = ${index},\n`;
             }
+            index++;
         }
-        let tmpArr: number[] = [];
-        for (let j = 0; j < titleLen; j++) {
-            one[j] = one[j].toString();
-            let tmpLen = getDisplayLength(one[j]);
-            tmpArr.push(tmpLen);
-            if (tmpLen > columnWidth[j]) {
-                columnWidth[j] = tmpLen;
-            }
-        }
-        widthArr[i] = tmpArr;
+        endStr += '}';
+
+        let csFilename = path.join(__dirname, serverPath);
+        fs.writeFileSync(csFilename, endStr);
     }
-    for (let i = 0; i < titleLen; i++) {
-        columnWidth[i] += 3;
-    }
-
-    for (let i = 0; i < infoArr.length; i++) {
-        let one = infoArr[i];
-        let tmpWidthArr = widthArr[i];
-        for (let j = 0; j < titleLen; j++) {
-            one[j] += " ".repeat(columnWidth[j] - tmpWidthArr[j]);
-        }
-        if (i === 0) {
-            console.log("\x1b[31m" + getRealStr(one.join("")) + "\x1b[0m");
-        } else {
-            console.log(getRealStr(one.join("")));
-        }
-    }
-
-
-
-    function getRealStr(str: string) {
-        while (getDisplayLength(str) > consoleMaxColumns) {
-            str = str.substring(0, str.length - 2);
-        }
-        return str;
-    }
-
-
-    //获得字符串实际长度，中文2，英文1
-    //控制台中中文占用2个英文字符的宽度
-    function getDisplayLength(str: string) {
-        let realLength = 0, len = str.length, charCode = -1;
-        for (var i = 0; i < len; i++) {
-            charCode = str.charCodeAt(i);
-            if (charCode >= 0 && charCode <= 128) {
-                realLength += 1;
-            } else {
-                realLength += 2;
-            }
-
-        }
-        return realLength;
-    }
-
 }
+
 
 function connectToMaster(host: string, port: number, token: string, cb: (client: clientProxy) => void) {
     console.log("try to connect  " + host + ":" + port);
